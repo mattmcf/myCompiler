a7e5832a2452137a05bb3e90ab4acf0d8582cafc
Matt McFarland <matthew.r.mcfarland.16@dartmouth.edu>
2016-02-04 20:41:54 -0500

    added variable declaration node handling

--- check_symbols/src/check_sym.c
+++ check_symbols/src/check_sym.c
@@ -21,6 +21,12 @@ extern int type_error_count;
  */
 int check_var_node(ast_node root);
 
+/* 
+ * returns 0 if all checks out
+ * returns 1 if type mismatch
+ */
+int check_var_declaration(root);
+
 /*
  * returns 1 if errors occurs
  * else returns 0 if all good
@@ -66,14 +72,26 @@ void set_type(ast_node root) {
 			root->mod 	= root->left_child->mod;
 			break;
 
+		case VAR_DECLARATION_N:
+			if (root->left_child->type == VOID_TS) {
+				type_err(root);
+				fprintf("cannot have void variables\n");
+			} 
+
+			break;
+
 		case VAR_DECL_N:
-			// can't declare void variables
-			// ** TODO **
-			// for any variables declared with initialization, make sure that initializing expression has matching type
+			if (check_var_declaration(root)) {
+				type_err(root);
+				fprintf("variable %s has improper type assignment\n",root->left_child->value_string);
+			}
 			break;
 
 		case FUNC_DECLARATION_N:
-			check_fdl_node(root);
+			if (check_fdl_node(root)) {
+				// type_errs() in gopher function
+				fprintf("function declaration for \'%s\' contains errors in body\n", root->left_child->right_sibling->value_string);
+			}		
 			break;
 
 		case EXPRESSION_N:
@@ -175,6 +193,7 @@ void set_type(ast_node root) {
 
 			if (check_var_node(root)) {
 				/* error occurred so set error values */
+				type_err(root);
 				root->type 	= NULL_TS;
 				root->mod 	= NULL_DT;
 			}
@@ -182,7 +201,11 @@ void set_type(ast_node root) {
 			break;
 
 		case RETURN_N:
-			assert(root->left_child);
+			root->type 	= root->left_child->type;
+			root->mod 	= root->left_child->mod;
+			break;
+
+		case ARG_LIST_N:
 			root->type 	= root->left_child->type;
 			root->mod 	= root->left_child->mod;
 			break;
@@ -309,6 +332,31 @@ int check_var_node(ast_node root) {
 	return 0;
 }
 
+/* 
+ * returns 0 if all checks out
+ * returns 1 if type mismatch
+ */
+int check_var_declaration(root) {
+	assert(root);
+
+	symnode_t * sym = lookup_symhashtable(root->scope_table, root->left_child->value_string, NOHASHSLOT);
+	if (!sym)
+		return 1;
+
+	type_specifier_t t	= sym->s.v.type;
+	modifier_t 	m	 	= sym->s.v.modifier;
+
+	if (root->left_child->right_sibling != NULL && root->left_child->right_sibling->type != INT_TS ) {
+
+		/* check initialization assignment */
+		if (root->left_child->right_sibling->type != t || root->left_child->right_sibling->mod != m) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
 /*
  * returns 1 if errors occurs
  * else returns 0 if all good
@@ -330,7 +378,7 @@ int check_fdl_node(ast_node root) {
 	if (found_return == 0) {
 
 		if (ret_type == VOID_TS) {
-			/* add a void return */
+			/* add a void return statement */
 
 			/* get compound statement node */
 			ast_node new_return;
@@ -348,6 +396,7 @@ int check_fdl_node(ast_node root) {
 				for (add_return_at = stmt->left_child; 
 						add_return_at->right_sibling != NULL;
 						add_return_at = add_return_at->right_sibling)
+					; 	// get to end of list
 
 				add_return_at->right_sibling = create_ast_node(RETURN_N);
 				assert(add_return_at->right_sibling);	
@@ -390,6 +439,8 @@ int gopher(type_specifier_t return_type, modifier_t mod_type, ast_node function_
 				root->line_number, TYPE_NAME(root->type), MODIFIER_NAME(root->mod), TYPE_NAME(return_type), MODIFIER_NAME(mod_type));
 			return 1;			
 		} else {
+
+			/* return statement checks out -- point to parent function */
 			root->parent_function = function_header;
 			return 0;
 		}
