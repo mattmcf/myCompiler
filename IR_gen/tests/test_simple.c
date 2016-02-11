3cc6baf686a6723b0a45bd983a3ca538461f74ed
Yondon Fu <y.fu710@gmail.com>
2016-02-10 21:51:06 -0500

    added psuedocode for most of the control flow statements as well as a bunch of the arithmetic operators

--- IR_gen/tests/test_simple.c
+++ IR_gen/tests/test_simple.c
@@ -4,6 +4,7 @@ int foo() {
 
 int main(void) {
   int a = 10;
+  int b;
 
   a = 7;
 
@@ -11,6 +12,14 @@ int main(void) {
   	print 7;
   } else {
   	print 0;
+  }
+
+  while (a == 7) {
+  	a = 1;
+  }
+
+  for (b = 0; b < 10; ++b) {
+  	print b;
   }
 
   return 0;
