9940680ab4635cd4157f3d2dd64c68bbf81347ee
Yondon Fu <y.fu710@gmail.com>
2016-02-21 19:07:13 -0500

    added test file for simple math operations

---
+++ Target_Code/tests/tmath.c
@@ -0,0 +1,32 @@
+int main(void) {
+	int a = 1;
+
+	a = a + 4;
+
+	print a; // 5
+
+	a = a - 2;
+
+	print a; // 3
+
+	a = a * 4; 
+
+	print a; // 12
+
+	a = a / 2;
+
+	print a; // 6
+
+	a = a % 5; 
+
+	print a; // 1
+
+	++a;
+
+	print a; // 2
+
+	--a;
+
+	print a; // 1
+
+	return 0;}
